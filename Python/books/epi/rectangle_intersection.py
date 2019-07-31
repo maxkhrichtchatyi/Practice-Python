6de7cb2ca15da7ab7ab936f34915b5900e5b096b
j0shu4b0y <maxim.khrichtchatyi@gmail.com>
2019-08-01 00:23:58 +0600

    Update - Fix algorithm and clean code.

--- Python/books/epi/rectangle_intersection.py
+++ Python/books/epi/rectangle_intersection.py
@@ -18,12 +18,14 @@ def intersect_rectangle(rectangle_1: 'Rectangle', rectangle_2: 'Rectangle') -> '
     if not is_intersect(rectangle_1, rectangle_2):
         return Rectangle(x=0, y=0, width=-1, height=-1)
 
-    x = max([rectangle_1.x, rectangle_2.x])
-    y = max([rectangle_1.y, rectangle_2.y])
-    width = max([rectangle_1.x + rectangle_1.width, rectangle_2.x + rectangle_2.width]) - x
-    height = max([rectangle_1.y + rectangle_1.height, rectangle_2.y + rectangle_2.height]) - y
+    x = max(rectangle_1.x, rectangle_2.x)
+    y = max(rectangle_1.y, rectangle_2.y)
+    width = min(rectangle_1.x + rectangle_1.width, rectangle_2.x + rectangle_2.width) - x
+    height = min(rectangle_1.y + rectangle_1.height, rectangle_2.y + rectangle_2.height) - y
 
     return Rectangle(x=x, y=y, width=width, height=height)
 
+rectangle_1 = Rectangle(x=0, y=0, width=1, height=1)
+rectangle_2 = Rectangle(x=0.5, y=0.5, width=1.5, height=1.5)
 
-print(intersect_rectangle(Rectangle(x=1, y=2, width=3, height=4), Rectangle(x=3, y=1, width=1, height=2)))
+print(intersect_rectangle(rectangle_1, rectangle_1))
