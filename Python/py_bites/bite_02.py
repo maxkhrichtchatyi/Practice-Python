3669cdb7d53daddf1668b9c4d6df8527ebf2b3d8
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2020-10-24 20:51:20 +0600

    feat: 💪 add python bite 02 regex_fun

--- Python/py_bites/bite_02.py
+++ Python/py_bites/bite_02.py
@@ -1,35 +1,62 @@
-# In this Bite we'll get you to take user input using the input builtin
-# and see if it matches items within a list.
+# Learn Python regular expressions by writing the following
+# 3 functions.
 #
-# In the while loop ask the user to enter a color and capture that input.
+# In each function's docstring we show the expected result.
 #
-# Check to see whether they've entered 'quit'. If so, print bye and exit
-# (break) the loop, effectively ending the function.
-#
-# If not, check to see whether the color (input) is in the VALID_COLORS
-# color list. If it is, then print the color in lower case. If not,
-# print Not a valid color and continue the loop.
-#
-# As you want to ask the user over and over again till they quit, you
-# can use an infinite while loop. We already provided that in the
-# template code, just code inside that loop.
+# The tests verify these expected results when you hit Save + Verify.
+# We will do a more ample regex bite later on.
+# Note that for parsing HTML you would use a library of some sort,
+# this is just to train your regex skills.
 
-VALID_COLORS = ["blue", "yellow", "red"]
+import re
+
+COURSE = (
+    "Introduction 1 Lecture 01:47"
+    "The Basics 4 Lectures 32:03"
+    "Getting Technical!  4 Lectures 41:51"
+    "Challenge 2 Lectures 27:48"
+    "Afterword 1 Lecture 05:02"
+)
+TWEET = (
+    "New PyBites article: Module of the Week - Requests-cache "
+    "for Repeated API Calls - http://pybit.es/requests-cache.html "
+    "#python #APIs"
+)
+HTML = (
+    "<p>pybites != greedy</p>"
+    "<p>not the same can be said REgarding ...</p>"
+)
 
 
-def print_colors():
-    """In the while loop ask the user to enter a color,
-       lowercase it and store it in a variable. Next check:
-       - if 'quit' was entered for color, print 'bye' and break.
-       - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
-       - otherwise print the color in lower case."""
-    while True:
-        color: str = input()
+def extract_course_times(course=COURSE):
+    """Return the course timings from the passed in
+       course string. Timings are in mm:ss (minutes:seconds)
+       format, so taking COURSE above you would extract:
+       ['01:47', '32:03', '41:51', '27:48', '05:02']
+       Return this list.
+    """
+    rule = re.compile(r"\d{2}:\d{2}")
+    return rule.findall(course)
 
-        if color == "quit":
-            print("bye")
-            break
-        elif color not in VALID_COLORS:
-            print("Not a valid color")
-        else:
-            print(color.lower())
+
+def get_all_hashtags_and_links(tweet=TWEET):
+    """Get all hashtags and links from the tweet text
+       that is passed into this function. So for TWEET
+       above you need to extract the following list:
+       ['http://pybit.es/requests-cache.html',
+        '#python',
+        '#APIs']
+       Return this list.
+    """
+    rule = re.compile(r"https?://\S+|\#\w+")
+    return rule.findall(tweet)
+
+
+def match_first_paragraph(html=HTML):
+    """Extract the first paragraph of the passed in
+       html, so for HTML above this would be:
+       'pybites != greedy' (= content of first paragraph).
+       Return this string.
+    """
+    rule = re.compile(r"<p>([^<]+)</p>")
+    return rule.findall(html)[0]
