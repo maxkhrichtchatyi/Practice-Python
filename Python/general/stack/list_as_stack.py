a2079ca68d11fbf5a4afb7563ea19204845060e4
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2021-02-15 02:14:58 +0600

    feat: 🏈 update general list as stack

---
+++ Python/general/stack/list_as_stack.py
@@ -0,0 +1,21 @@
+# init stack
+stack: list = []
+
+# add values to the stack
+stack.append("eat")
+stack.append("sleep")
+stack.append("code")
+
+print(f"Stack values: {stack}")
+
+# pull the last value from the stack
+last_value = stack.pop()
+
+print(f"The last value from the stack: {last_value}")
+
+print(f"Stack values: {stack}")
+
+# pull the last value from the stack
+last_value = stack.pop()
+
+print(f"Stack values: {stack}")
