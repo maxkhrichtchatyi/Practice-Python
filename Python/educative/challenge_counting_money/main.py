1be3ec1d85fdb8bdbfe3ad0a663ee1643d099fa1
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2023-11-28 18:39:01 +0600

    feat: 💪 add Counting Money challenge

---
+++ Python/educative/challenge_counting_money/main.py
@@ -0,0 +1,18 @@
+def find_min_coins(v, coins_available):
+    """
+    This function finds the minimum number of coins
+    :param v: Total amount
+    :param coins_available: Coins available in the machine
+    :return: A list of total coins
+    """
+
+    result = []
+
+    for coin in reversed(coins_available):
+        mod_remainder = v // coin
+        if mod_remainder:
+            list_of_coins = [coin] * mod_remainder
+            result.extend(list_of_coins)
+            v -= sum(list_of_coins)
+
+    return result
